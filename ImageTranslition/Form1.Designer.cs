00e6b5d921541a8c1bf907d4a1e0d5271bb68373
Alcopro <Outcaster@mail.ru>
2016-01-06 21:07:54 +0300

    more

--- ImageTranslition/Form1.Designer.cs
+++ ImageTranslition/Form1.Designer.cs
@@ -28,28 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.ResultPicture = new System.Windows.Forms.PictureBox();
+            this.WorkPicture = new System.Windows.Forms.PictureBox();
+            this.SourcePicture = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.FinalPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorkPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SourcePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FinalPicture)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(482, 579);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
@@ -71,7 +68,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Repaint";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -79,24 +76,13 @@
             // 
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(544, 474);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(482, 579);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Visible = false;
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(544, 117);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Zoom x2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -106,41 +92,122 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
+            this.button3.Text = "Zoom x0.5";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ResultPicture
+            // 
+            this.ResultPicture.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ResultPicture.Location = new System.Drawing.Point(12, 6);
+            this.ResultPicture.Name = "ResultPicture";
+            this.ResultPicture.Size = new System.Drawing.Size(512, 565);
+            this.ResultPicture.TabIndex = 7;
+            this.ResultPicture.TabStop = false;
+            // 
+            // WorkPicture
+            // 
+            this.WorkPicture.Location = new System.Drawing.Point(758, 12);
+            this.WorkPicture.Name = "WorkPicture";
+            this.WorkPicture.Size = new System.Drawing.Size(100, 50);
+            this.WorkPicture.TabIndex = 8;
+            this.WorkPicture.TabStop = false;
+            this.WorkPicture.Visible = false;
+            // 
+            // SourcePicture
+            // 
+            this.SourcePicture.Location = new System.Drawing.Point(758, 68);
+            this.SourcePicture.Name = "SourcePicture";
+            this.SourcePicture.Size = new System.Drawing.Size(100, 50);
+            this.SourcePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.SourcePicture.TabIndex = 9;
+            this.SourcePicture.TabStop = false;
+            this.SourcePicture.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(530, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(286, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(544, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(625, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FinalPicture
+            // 
+            this.FinalPicture.Location = new System.Drawing.Point(758, 521);
+            this.FinalPicture.Name = "FinalPicture";
+            this.FinalPicture.Size = new System.Drawing.Size(100, 50);
+            this.FinalPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.FinalPicture.TabIndex = 13;
+            this.FinalPicture.TabStop = false;
+            this.FinalPicture.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(870, 615);
+            this.Controls.Add(this.FinalPicture);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.SourcePicture);
+            this.Controls.Add(this.WorkPicture);
+            this.Controls.Add(this.ResultPicture);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorkPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SourcePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FinalPicture)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox ResultPicture;
+        private System.Windows.Forms.PictureBox WorkPicture;
+        private System.Windows.Forms.PictureBox SourcePicture;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox FinalPicture;
     }
 }
 
--- ImageTranslition/Form1.cs
+++ ImageTranslition/Form1.cs
@@ -41,12 +41,12 @@ namespace ImageTranslition
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Zoom(pictureBox1, 0.5);
+            Zoom(ResultPicture, 2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Zoom(pictureBox1, 1);
+            Zoom(ResultPicture, 0.5);
         }
 
         int min(int x, int y)
@@ -88,6 +88,59 @@ namespace ImageTranslition
             return res;
         }
 
+        bool validPicture;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string fle = openFileDialog1.FileName;
+            if (fle.Substring(fle.LastIndexOf('.') + 1, fle.Length - fle.LastIndexOf('.') - 1) == "jpg")
+            {
+                validPicture = true;
+                textBox3.Text = openFileDialog1.FileName;
+            }
+            else
+            {
+                validPicture = false;
+                MessageBox.Show("Invalid FileName!");
+            }
+        }
+
+        private void DrawResultPicture(PictureBox FromPicture)
+        {
+            int NewPicWidth = ResultPicture.Width;
+            double koef = (double)ResultPicture.Width / (double)FromPicture.Width;
+            int NewPicHeight = (int)((double)FromPicture.Height * koef);
+
+            ResultPicture.Width = NewPicWidth;
+            ResultPicture.Height = NewPicHeight;
+
+            Bitmap tempBmp = new Bitmap(NewPicWidth, NewPicHeight);
+            Graphics grp = Graphics.FromImage(tempBmp);
+            Bitmap bmp = new Bitmap(FromPicture.Image);
+            Rectangle srcRect = new Rectangle(0, 0, FromPicture.Width, FromPicture.Height);
+            Rectangle dstRect = new Rectangle(0, 0, NewPicWidth, NewPicHeight);
+            grp.DrawImage(bmp, dstRect, srcRect, GraphicsUnit.Pixel);
+            grp.Dispose();
+
+            ResultPicture.Image = tempBmp;
+            ResultPicture.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!validPicture)
+            {
+                MessageBox.Show("Invalid FileName!");
+                return;
+            }
+            WorkPicture.Image = Image.FromFile(textBox3.Text);
+            SourcePicture.Image = Image.FromFile(textBox3.Text);
+            FinalPicture.Image = Image.FromFile(textBox3.Text);
+
+            DrawResultPicture(SourcePicture);
+        }
+
         public Color GetAverageColor(int j, int i, int j1, int i1)
         {
             /* Color c1, c2, c3, c4;
@@ -100,9 +153,9 @@ namespace ImageTranslition
              int AverageGreen = (c1.G + c2.G + c3.G + c4.G) / 4;
              int AverageBlue = (c1.B + c2.B + c3.B + c4.B) / 4;
              return Color.FromArgb(AverageRed, AverageGreen, AverageBlue);*/
-            int AverageRed   = (pxl[0, i, j] + pxl[0, i1, j] + pxl[0, i, j1] + pxl[0, i1, j1]) / 4;
+            int AverageRed = (pxl[0, i, j] + pxl[0, i1, j] + pxl[0, i, j1] + pxl[0, i1, j1]) / 4;
             int AverageGreen = (pxl[1, i, j] + pxl[1, i1, j] + pxl[1, i, j1] + pxl[1, i1, j1]) / 4;
-            int AverageBlue  = (pxl[2, i, j] + pxl[2, i1, j] + pxl[2, i, j1] + pxl[2, i1, j1]) / 4;
+            int AverageBlue = (pxl[2, i, j] + pxl[2, i1, j] + pxl[2, i, j1] + pxl[2, i1, j1]) / 4;
 
             return Color.FromArgb(AverageRed, AverageGreen, AverageBlue);
 
@@ -118,33 +171,59 @@ namespace ImageTranslition
                 {
                     var brush = new SolidBrush
                     (GetAverageColor
-                    (min(j + 1, GlobW - 1),
-                     min(i + 1, GlobH - 1),
-                     min(j + scaledw - 1, GlobW - 1),
-                     min(i + scaledh - 1, GlobH - 1)));
-                    grp.FillRectangle(brush, new Rectangle(j, i, scaledw, scaledh));
+                    (min(j, GlobW - 1),
+                     min(i, GlobH - 1),
+                     min(j + scaledw, GlobW - 1),
+                     min(i + scaledh, GlobH - 1)));
+                    grp.FillRectangle(brush, new Rectangle(min(j, GlobW), min(i, GlobH), min(j + scaledw, GlobW), min(i + scaledh, GlobH)));
                 }
                 return;
             //var brush = new SolidBrush(Color.Black);
             //grp.FillRectangle(brush, new Rectangle(0, 0, 200, 200));
         }
 
-        public void Zoom(PictureBox pb1, double x)
-        {
-            Graphics grp = Graphics.FromHwnd(pb1.Handle);
-            Bitmap bmp = new Bitmap(pb1.Image);
-            int newW = (int)(bmp.Width * x);
-            int newH = (int)(bmp.Height * x);
-            Rectangle srcRect = new Rectangle(0, 0, newW, newH);
-            Rectangle dstRect = new Rectangle(0, 0, pb1.Width, pb1.Height);
-            grp.DrawImage(bmp, dstRect, srcRect, GraphicsUnit.Pixel);
-            return;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Interfase thr = new Interfase(Work);
             IAsyncResult res = thr.BeginInvoke(null, null);
+            //Work();
+        }
+
+        private void Rescale(PictureBox Pic, double scale)
+        {
+
+            Bitmap bmp = new Bitmap(Pic.Image);
+            int newW = (int)(bmp.Width * scale);
+            int newH = (int)(bmp.Height * scale);
+            Pic.Width = newW;
+            Pic.Height = newH;
+
+            Bitmap tempBmp = new Bitmap(newW, newH);
+            Graphics grp = Graphics.FromImage(tempBmp);
+            Rectangle srcRect = new Rectangle(0, 0, newW, newH);
+            Rectangle dstRect = new Rectangle(0, 0, Pic.Width, Pic.Height);
+            grp.DrawImage(bmp, dstRect, srcRect, GraphicsUnit.Pixel);
+            grp.Dispose();
+
+            Pic.Image = tempBmp;
+            return;
+        }
+
+        private void Zoom(PictureBox Pic, double scale)
+        {
+            Bitmap bmp = new Bitmap(WorkPicture.Image);
+            int newW = (int)(Pic.Width * scale);
+            int newH = (int)(Pic.Height * scale);
+
+            Bitmap tempBmp = new Bitmap(Pic.Image);
+            Graphics grp = Graphics.FromImage(tempBmp);
+            Rectangle srcRect = new Rectangle(0, 0, newW, newH);
+            Rectangle dstRect = new Rectangle(0, 0, Pic.Width, Pic.Height);
+            grp.DrawImage(bmp, dstRect, srcRect, GraphicsUnit.Pixel);
+            grp.Dispose();
+
+            Pic.Image = tempBmp;
+            return;
         }
 
         private void Work()
@@ -153,33 +232,37 @@ namespace ImageTranslition
             //MessageBox.Show(Convert.ToString(int.Parse(textBox2.Text)));
             double h = double.Parse(textBox1.Text);
             double w = double.Parse(textBox2.Text);
+            if (h == 0 || w == 0)
+                return;
             scale = 1;
             if (h < 1 || w < 1)
                 scale = 10;
             if (h < 0.1 || w < 0.1)
                 scale = 100;
-            scaledh = (int)(h * scale);
-            scaledw = (int)(w * scale);
-            pattern = new Bitmap(pictureBox1.Height, pictureBox1.Width);
-            pattern = (Bitmap)pictureBox2.Image;
+            if (h < 0.01 || w < 0.01)
+                scale = 1000;
+            if (h < 0.001 || w < 0.001)
+                scale = 10000;
 
-            koefh = (double)pictureBox1.Height / (double)pictureBox1.Image.Height;
-            koefw = (double)pictureBox1.Width / (double)pictureBox1.Image.Width;
+            scaledh = (int)h * scale;
+            scaledw = (int)w * scale;
 
-            scaledh = (int)(scaledh * 1 / koefh);
-            scaledw = (int)(scaledw * 1 / koefw);
+            Bitmap WorkBitmap = new Bitmap(SourcePicture.Image);
 
-            bmp = new Bitmap(pictureBox1.Height, pictureBox1.Width);
-            bmp = (Bitmap)pictureBox1.Image;
+            WorkPicture.Image = WorkBitmap;
+            Rescale(WorkPicture, scale);
 
-            bmp.SetResolution(pictureBox1.Image.VerticalResolution, pictureBox1.Image.HorizontalResolution);
-            grp = Graphics.FromImage(bmp);
+            WorkPicture.Refresh();
 
-            GlobH = bmp.Height;
-            GlobW = bmp.Width;
+            GlobH = WorkPicture.Height;
+            GlobW = WorkPicture.Width;
+
+            WorkBitmap = new Bitmap(WorkPicture.Image);
+
+            grp = Graphics.FromImage(WorkBitmap);
 
             pxl = new byte[3, GlobH, GlobW];
-            pxl = BitmapToByteRgb(pattern);
+            pxl = BitmapToByteRgb(WorkBitmap);
 
             for (int i = 0; i < GlobH; i += scaledh)
             {
@@ -194,10 +277,16 @@ namespace ImageTranslition
 
             }
 
+            WorkPicture.Image = WorkBitmap;
+            FinalPicture.Image = WorkBitmap;
+
+            MessageBox.Show("Successful!");
+            DrawResultPicture(WorkPicture);
+
             Action action = () =>
             {
-                pictureBox1.Image = bmp;
-                pictureBox1.Refresh();
+                ResultPicture.Image = bmp;
+                ResultPicture.Refresh();
             };
             Invoke(action);
         }
